4fafbdf62f1c0dfd539b30706ab6a6e70e0ff325
monsterreh2b <brant.han@gmail.com>
2017-04-01 21:48:24 -0700

    Functions but missing win alert, counter, and reset

--- resources/javascript/game.js
+++ resources/javascript/game.js
@@ -1,20 +1,24 @@
-var words = ['cat', 'tree', 'swing', 'around', 'scientist'];
-var wins = 0;
+var words = ["cat", "tree", "swing", "around", "scientist"];
+var wins = [];
 var wordGuess = words[Math.floor(Math.random() * words.length)];
 var blanks = [];
 var userGuessTotal= 12;
 var guessed = [];
+var winCount = 0;
+var counter= 0;
 
-
-
+//convert guess(wordGuess) into successive blanks on screen
 function wordBlank(guess){
 for (var i=0; i<guess.length; i++){
     blanks[i] = "_";
-}
+}//join a space in between blanks array for formatting
 document.querySelector("#display").innerHTML = blanks.join(" ");
 }
 
 
+//compare guess[i] (until guess.length) with userChoice
+//if match get previous blanks array and inject match at the 
+//corresponding blanks array position
 function loop(guess, userKey){
 
 var match=0;
@@ -22,18 +26,31 @@ var match=0;
 for (var i = 0; i<guess.length;i++){
     if (guess[i] === userKey){
         blanks[i] = userKey;
-        match+=1;
-       
+        // wins.push(userkey);
+
+     
       }
        
-    }
+}
 
 
 
+
+
+//grow array for letters already guessed
 guessed.push(userKey);
+
 console.log(blanks);
+
+
+
+
 document.querySelector("#display").innerHTML = blanks.join(" ");
 document.querySelector("#remaining").innerHTML = --userGuessTotal;
+//   if ((guessed.length<=12) && (wins.Length===wordGuess.length)){
+//     alert("You Win!");
+//     document.querySelector("#wins").innerHTML = winCount++;
+// }else 
 if (guessed.length<=12){
 document.querySelector("#guessed").innerHTML = guessed.join("  ");
 
@@ -41,11 +58,32 @@ document.querySelector("#guessed").innerHTML = guessed.join("  ");
   alert("You lost! Choose another word");
   location.reload();
 }
-
+// if ((blanks.length===guess.length) && (guess.length<=12)){
+// document.querySelector("#wins").innerHTML = ++wins;
+// }
 
 
 }
 
+function winCheck(guess, array2){
+
+  if ( JSON.stringify(guess) === JSON.stringify(array2)){
+// for (var i=0;i< guess.length; i++) {
+//     for (var j=0;j< array2.length; j++) {
+//         if (array1[i] === array2[j]) {
+//             counter=counter+1;
+
+//         }
+//     }
+// }
+// if (counter===array2.length){
+//   alert("You Win!");
+// document.querySelector("#wins").innerHTML = winCount++;
+// }
+alert("You Win!");
+document.querySelector("#wins").innerHTML = winCount++;
+}
+}
 
 wordBlank(wordGuess);
 
@@ -56,46 +94,6 @@ var userChoice = event.key;
    
 
 loop(wordGuess,userChoice);
-
-
+winCheck(wordGuess,blanks);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-    // document.onkeyup = function(event) {
-
-    //     var userGuess = event.key;
-        
-
-    //     // Reworked our code from last step to use "else if" instead of lots of if statements.
-        
-
-
-
-   
-    
-    // if (count===word.length){
-    //   wins++;
-    
-
-    //       var html = "<p>Press any key to get started!</p>" +
-
-    //       "<p>Wins: " + wins + "</p>" +
-    //       "<p>Number of guesses remaining: " + userGuessTotal + "</p>" +
-    //       "<p>Letters already guessed: " + guessed + "</p>";
-
-    //       document.querySelector("#stats").innerHTML = html;
-        
-
-
-    //   }
